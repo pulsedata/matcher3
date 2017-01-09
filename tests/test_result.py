aed80c35c672fce12f97fcef9abd587f73380175
Sergio <sergiomattei@outlook.com>
2017-01-09 18:24:35 -0400

    Fix tests for Result

--- tests/test_result.py
+++ tests/test_result.py
@@ -12,5 +12,5 @@ def test_correct_insert():
     assert len(r.high) == 1
     r.insert(50, "Medium")
     assert len(r.medium) == 1
-    r.insert(1, "Low")
+    r.insert(30, "Low")
     assert len(r.low) == 1
